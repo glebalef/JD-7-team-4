afbbba93bcaa98a1603d42ed1d216b27318d67ae
glebalef <gleb.alef@gmail.com>
2022-11-19 19:00:55 +0300

    - добавлен метод, реагирущий на команду /start - добавлена клавиутра для начального экрана

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,14 +2,23 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.Keyboard;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -27,10 +36,26 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
-        });
+                    logger.info("Processing update: {}", update);
+                    // Process your updates here
+                    if (Objects.equals(update.message().text(), "/start")) {
+                        SendResponse response = telegramBot.execute(initialMessage(update)
+                                .replyMarkup(initialKeyboard));
+                    };
+        }
+        );
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+
+    // изначальное сообщение для пользователя после команды /Start
+    public SendMessage initialMessage (Update update) {
+        return new SendMessage(update.message().chat().id(), "Вас приветствует бот Кожуховского приюта для собак. Пожалуйста, выберете интересующий Вас раздел");
+    }
+
+
+    // Клавиатура для начального экрана - после сообщения /start
+    Keyboard initialKeyboard = new ReplyKeyboardMarkup(
+            new String[]{"Узнать информацию о приюте", "Как взять собаку из приюта "},
+            new String[]{"Прислать отчет о питомце", "Позвать волонтера"});
 }
