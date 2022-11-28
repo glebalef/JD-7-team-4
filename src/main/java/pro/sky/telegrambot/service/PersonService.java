fdc7a326cf6421d8726e995e773601c863039abf
Evgeny Fisenko <evgeny.f.aircelle@gmail.com>
2022-11-28 18:05:32 +0200

    Создал сущность пользователя и запись его в базу. Join колонку dog_id в таблицу person.

---
+++ src/main/java/pro/sky/telegrambot/service/PersonService.java
@@ -0,0 +1,31 @@
+package pro.sky.telegrambot.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.Models.Person;
+import pro.sky.telegrambot.repository.PersonRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    //метод присвоения собаки усыновителю (нужен буде контроллер)
+
+    public Person attachDog(Person person) {
+        Optional<Person> adopter = personRepository.findById(person.getChatId());
+        if (adopter.isPresent()) {
+            person.setDog(person.getDog());
+        }
+        return person;
+    }
+
+
+}
