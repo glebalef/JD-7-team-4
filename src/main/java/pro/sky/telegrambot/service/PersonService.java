15afc7ff2067620b1cf897af6eec74954164ee6c
Evgeny Fisenko <evgeny.f.aircelle@gmail.com>
2022-11-30 16:08:44 +0200

    Покрыл комментариями классы - Person - PersonService - PersonRepository

--- src/main/java/pro/sky/telegrambot/service/PersonService.java
+++ src/main/java/pro/sky/telegrambot/service/PersonService.java
@@ -1,13 +1,16 @@
 package pro.sky.telegrambot.service;
 
+import com.pengrad.telegrambot.model.Update;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.Models.Person;
 import pro.sky.telegrambot.Repository.PersonRepository;
 
-import java.util.Optional;
-
+/**
+ * сервис-класс для работы с сущностью Person
+ *
+ * @author Евгений Фисенко
+ */
 @Service
-
 public class PersonService {
 
     private final PersonRepository personRepository;
@@ -16,15 +19,24 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    //метод присвоения собаки усыновителю (нужен буде контроллер)
+    /**
+     * получает имя и фамилию пользователя и, если это новый пользователь, заносит пользователя в базу
+     *
+     * @param update - данные о сообщении из класса TelegramBotUpdateListener
+     * @return возвращает пользователя
+     */
+    public Person getPersonByChatId(Update update) {
 
-    public Person attachDog(Person person) {
-        Optional<Person> adopter = personRepository.findById(person.getChatId());
-        if (adopter.isPresent()) {
-            person.setDog(person.getDog());
+        Person newPerson = new Person();
+
+        if (personRepository.findByChatId(update.message().chat().id()) == null) {
+
+            newPerson.setFirstName(update.message().chat().firstName());
+            newPerson.setLastName(update.message().chat().lastName());
+            newPerson.setChatId(update.message().chat().id());
+            personRepository.save(newPerson);
         }
-        return person;
+
+        return newPerson;
     }
-
-
 }
